545bc03586d0e49779c37e0049731cd46ce63585
Samuel Pereira <samuel.pereira.70870@gmail.com>
2026-02-05 12:32:36 -0700

    W05 Prepare: Learning Activity

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -5,5 +5,22 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning03 World!");
+
+        Fraction myFract = new Fraction();
+
+        for (int i = 1; i <= 20; i++)
+        {
+            Random randomObject = new Random();
+            int _randomTop = randomObject.Next(200) + 1;
+            int _randomBottom = randomObject.Next(50) + 1;
+
+            myFract.SetTop(_randomTop);
+            myFract.SetBottom(_randomBottom);
+
+            string stg = myFract.GetFractionString();
+            double dbl = myFract.GetDecimalValue();
+
+            Console.WriteLine($"Fraction {i}: string: {stg} Number: {dbl:F2}");
+        }
     }
 }
